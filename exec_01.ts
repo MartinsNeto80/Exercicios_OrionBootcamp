46f7f748132221f6a036d26d1d46512535f95afc
MartinsNeto80 <martinsneto.8080@gmail.com>
2023-10-11 13:55:41 -0300

    Adicionando ; seguindo orientações de boas praticas.

--- exec_01.ts
+++ exec_01.ts
@@ -1,22 +1,22 @@
 //Exercicio 01 - A
 function contadorVogais(nome: string) {
-    const vogais: string[] = ['a', 'e', 'i', 'o', 'u']
-    const letras: Array<string> = nome.toLowerCase().split('')
-    let qnt = 0
+    const vogais: string[] = ['a', 'e', 'i', 'o', 'u'];
+    const letras: Array<string> = nome.toLowerCase().split('');
+    let qnt = 0;
     for (let vogal in vogais) {
 
         for (let letra in letras) {
 
             if (vogais[vogal] === letras[letra]){
                 
-                qnt++
+                qnt++;
                  
             }
 
         }
 
     }
-    return qnt
+    return qnt;
 }
 
 //var contagem = contadorVogais('Adna Patricia')
@@ -24,14 +24,14 @@ function contadorVogais(nome: string) {
 
 //Exercicio 01 - B
 if (typeof window !== "undefined") {
-const entrada = document.getElementById("entrada") as HTMLInputElement
-const button = document.getElementById("button") as HTMLInputElement
-const res = document.getElementById("res") as HTMLInputElement
+const entrada = document.getElementById("entrada") as HTMLInputElement;
+const button = document.getElementById("button") as HTMLInputElement;
+const res = document.getElementById("res") as HTMLInputElement;
 
 button.addEventListener("click", () => {
     const nome = entrada.value
-    const contador = contadorVogais(nome)
-    res.innerText = `A quantidade de vogais existentes no nome ${nome} é ${contador}.`
-    return res.innerText
-})
+    const contador = contadorVogais(nome);
+    res.innerText = `A quantidade de vogais existentes no nome ${nome} é ${contador}.`;
+    return res.innerText;
+});
 }
