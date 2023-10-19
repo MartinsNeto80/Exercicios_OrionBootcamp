d6e4f7d687fa6faf7cd8ed9348326f9dcd86b180
MartinsNeto80 <martinsneto.8080@gmail.com>
2023-10-19 10:19:46 -0300

    fix: Correções informadas no PR

--- exec_02.js
+++ exec_02.js
@@ -1,69 +1,78 @@
 var lista = [
-    { id: 1, name: "Ada Lovelace", bio: "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina" },
-    { id: 2, name: "Alan Turing", bio: "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial" },
-    { id: 3, name: "Nikola Tesla", bio: "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada." },
-    { id: 4, name: "Nicolau Copérnico", bio: "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }
+    {
+        id: 1,
+        name: "Ada Lovelace",
+        bio: "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina",
+    },
+    {
+        id: 2,
+        name: "Alan Turing",
+        bio: "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial",
+    },
+    {
+        id: 3,
+        name: "Nikola Tesla",
+        bio: "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada.",
+    },
+    {
+        id: 4,
+        name: "Nicolau Copérnico",
+        bio: "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar.",
+    },
 ];
 //a) Crie uma função que retorne a bio do id passado
 function buscaBio(numeroID) {
     var object = lista.find(function (object) { return object.id === numeroID; });
     console.log(object === null || object === void 0 ? void 0 : object.bio);
-    return object ? object.bio : "ID inexistente.";
+    return object ? object.bio : undefined;
 }
 if (typeof window !== "undefined") {
-    var entradaa_1 = document.getElementById("entradaa");
-    var buttona = document.getElementById("buttona");
-    var resa_1 = document.getElementById("resa");
-    buttona.addEventListener("click", function () {
-        var numeroID = Number(entradaa_1.value);
+    var entradaIdFir_1 = document.getElementById("entradaa");
+    var buttonAtivarFir = document.getElementById("buttona");
+    var resA_1 = document.getElementById("resa");
+    buttonAtivarFir.addEventListener("click", function () {
+        var numeroID = Number(entradaIdFir_1.value);
         var bio = buscaBio(numeroID);
-        if (bio === "ID inexistente.") {
-            resa_1.innerText = "".concat(bio);
-            return resa_1.innerText;
-        }
-        else {
-            resa_1.innerText = "A bio do ID selecionado \u00E9 \"".concat(bio, "\".");
-            return resa_1.innerText;
-        }
+        return (resA_1.innerText = bio
+            ? "A bio do ID selecionado \u00E9 ".concat(bio, ".")
+            : "ID Inexistente.");
     });
 }
 //b) Crie uma função que retorne o name do id passado
 function buscaName(numeroID) {
     var object = lista.find(function (object) { return object.id === numeroID; });
-    //console.log(object?.name);    
-    return object ? object.name : "ID inexistente.";
+    //console.log(object?.name);
+    return object ? object.name : undefined;
 }
 if (typeof window !== "undefined") {
-    var entradab_1 = document.getElementById("entradab");
-    var buttonb = document.getElementById("buttonb");
-    var resb_1 = document.getElementById("resb");
-    buttonb.addEventListener("click", function () {
-        var numeroID = Number(entradab_1.value);
+    var entradaIdSec_1 = document.getElementById("entradab");
+    var buttonAtivarSec = document.getElementById("buttonb");
+    var resB_1 = document.getElementById("resb");
+    buttonAtivarSec.addEventListener("click", function () {
+        var numeroID = Number(entradaIdSec_1.value);
         var nome = buscaName(numeroID);
-        if (nome === "ID inexistente.") {
-            resb_1.innerText = "".concat(nome);
-            return resb_1.innerText;
+        if (nome === undefined) {
+            return (resB_1.innerText = "".concat(nome));
         }
         else {
-            resb_1.innerText = "A bio do ID selecionado \u00E9 \"".concat(nome, "\".");
-            return resb_1.innerText;
+            return (resB_1.innerText = "A bio do ID selecionado \u00E9 \"".concat(nome, "\"."));
         }
     });
 }
 //c) Crie uma função que apague um item da lista a partir de um id passado
 function delObjeto(numeroID) {
-    var object = lista.filter(function (object) { return object.id !== numeroID; });
-    return object ? object : "ID inexistente.";
+    var objectDel = lista.filter(function (object) { return object.id !== numeroID; });
+    return objectDel ? objectDel : undefined;
 }
 if (typeof window !== "undefined") {
-    var entradac_1 = document.getElementById("entradac");
-    var buttonc = document.getElementById("buttonc");
-    var resc_1 = document.getElementById("resc");
-    buttonc.addEventListener("click", function () {
-        var numeroID = Number(entradac_1.value);
-        var contador = delObjeto(numeroID);
-        resc_1.innerText = "O id: ".concat(numeroID, " foi deletado com sucesso. ").concat(JSON.stringify(contador));
-        return resc_1.innerText;
+    var entradaIdThi_1 = document.getElementById("entradac");
+    var buttonAtivarThi = document.getElementById("buttonc");
+    var resC_1 = document.getElementById("resc");
+    buttonAtivarThi.addEventListener("click", function () {
+        var numeroID = Number(entradaIdThi_1.value);
+        var listaFinal = delObjeto(numeroID);
+        resC_1.innerText = "O id: ".concat(numeroID, " foi deletado com sucesso. ").concat(JSON.stringify(listaFinal));
+        return resC_1.innerText;
     });
 }
 //d) Crie uma função que altere a bio ou o name a partir de um id passado
@@ -85,15 +94,15 @@ function alterarNomeBio(numeroID, mudar, textMudar) {
     }
 }
 if (typeof window !== "undefined") {
-    var entradad1_1 = document.getElementById("entradad1");
-    var entradad2_1 = document.getElementById("entradad2");
-    var entradad3_1 = document.getElementById("entradad3");
-    var buttond = document.getElementById("buttond");
+    var entradaIdFou_1 = document.getElementById("entradad1");
+    var entradaTxtBio_1 = document.getElementById("entradad2");
+    var entradaTxtName_1 = document.getElementById("entradad3");
+    var buttonAtivarFou = document.getElementById("buttond");
     var resd_1 = document.getElementById("resd");
-    buttond.addEventListener("click", function () {
-        var numeroID = Number(entradad1_1.value);
-        var nomeBio = entradad2_1.value;
-        var texto = entradad3_1.value;
+    buttonAtivarFou.addEventListener("click", function () {
+        var numeroID = Number(entradaIdFou_1.value);
+        var nomeBio = entradaTxtBio_1.value;
+        var texto = entradaTxtName_1.value;
         var contador = alterarNomeBio(numeroID, nomeBio, texto);
         resd_1.innerText = "".concat(JSON.stringify(contador));
         return resd_1.innerText;
@@ -115,11 +124,11 @@ function buscaBio(numeroID: number){
     }
 }
 ================= FUNCIONAL =======================
-function buscaBio(numeroID: number): string{
-    const object = lista.find((object) => object.id === numeroID);
-    //console.log(object?.bio);
-    return object ? object.bio : "ID inexistente.";
- }
+function buscaBio(numeroID: number): string | undefined {
+  const object = lista.find((object) => object.id === numeroID);
+  console.log(object?.bio);
+  return object ? object.bio : undefined;
+}
 
 
 
@@ -133,11 +142,11 @@ function buscaName(numeroID: number){
     }
 }
 ================= FUNCIONAL =======================
-function buscaName(numeroID: number): string{
-    const object = lista.find((object) => object.id === numeroID);
-    //console.log(object?.name);
-    return object ? object.name : "ID inexistente.";
- }
+function buscaName(numeroID: number): string | undefined {
+  const object = lista.find((object) => object.id === numeroID);
+  //console.log(object?.name);
+  return object ? object.name : undefined;
+}
 
 
 
@@ -151,9 +160,9 @@ function delObjeto(numeroID: number){
     }
 }
 ================= FUNCIONAL =======================
-function delObjeto(numeroID: number){
-    const object = lista.findIndex((object) => object.id === numeroID);
-    return object ? lista.splice(object,1) : "ID inexistente.";
+function delObjeto(numeroID: number) {
+  const objectDel = lista.filter((object) => object.id !== numeroID);
+  return objectDel ? objectDel : undefined;
 }
 
 
@@ -182,4 +191,4 @@ function alterarNomeBio(numeroID: number, mudar: "name" | "bio", textMudar: stri
             //console.log("ID inexistente.");
             return "ID inexistente.";
         }
- }*/ 
+ }*/
